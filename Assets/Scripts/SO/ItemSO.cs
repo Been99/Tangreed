7c570327d07090217cfdc8b29dc37e578917fcda
Been99 <167202496+Been99@users.noreply.github.com>
2024-06-20 00:40:13 +0900

    [Complex] 인벤토리 UI 틀 생성 및 아이템 타입 설정 중

---
+++ Assets/Scripts/SO/ItemSO.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+
+[Serializable]
+public class ItemStat
+{
+    public EItemStat itemStat;
+    public float statValue;
+}
+
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "SO/ItemSO")]
+public class ItemSO : ScriptableObject
+{
+    [Header("Info")]
+    public string itemName; // 아이템 이름
+    public string itemDescripttion; // 아이템 설명
+    public EItemType itemType; // 아이템 타입
+    public Sprite icon; // 아이템 이미지
+
+    [Header("ItemStat")]
+    public ItemStat[] itemStats;
+
+    [Header("ItemPrice")]
+    public int itemPrice; // 아이템 가격
+
+    // TODO : 더 필요한 내용 확인 후 적용
+}
